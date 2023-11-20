b899580e24fdd15003bb9b2ca9352aba4e38788f
LeviDias1 <79313828+LeviDias1@users.noreply.github.com>
2023-11-20 11:05:19 -0300

    Delete src directory

--- src/Routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const UserController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getUserById);
-
-module.exports = router;
